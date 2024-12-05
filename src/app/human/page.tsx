34d8ad2b4976771dbf6893be14dfb871b7d4f90f
Lohan levis <lohanlevis@gmail.com>
2024-12-06 00:30:29 +0100

    ajout de l'humain et de sa page

--- src/app/human/page.tsx
+++ src/app/human/page.tsx
@@ -2,13 +2,27 @@ import Image from 'next/image';
 
 export default function Humain() {
     return (
-        <div className="flex justify-center items-center h-screen">
-            <Image 
-                src="/img/human.svg" 
-                alt="Human SVG" 
-                width={500} 
-                height={500} 
-            />
+        <div 
+            className="flex justify-center items-center flex-1"
+            style={{
+                backgroundImage: "url('/img/background.svg')",
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+            }}
+        >
+            <div className="flex justify-center items-center" style={{ maxHeight: '80vh' }}>
+                <Image 
+                    src="/img/human.svg" 
+                    alt="Human SVG" 
+                    width={0}  
+                    height={0} 
+                    style={{
+                        width: 'auto',   
+                        height: 'auto',  
+                        maxHeight: '70vh' 
+                    }}
+                />
+            </div>
         </div>
     );
 }
