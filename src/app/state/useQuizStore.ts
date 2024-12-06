45fd67ddfde7a99dc715946635c9c6fb70eeefaf
Enzo Legros <enzo.legros@isaan.fr>
2024-12-06 04:46:12 +0100

    qcm popup done and liked to store

--- src/app/state/useQuizStore.ts
+++ src/app/state/useQuizStore.ts
@@ -1,70 +1,79 @@
-import { create } from 'zustand';
-import type { SubjectType } from '~/app/enum/SubjectType';
-import type { HumanBodyPosition } from '~/app/enum/HumanBodyPosition';
-import type { Question } from '~/app/enum/Question';
-import initialQuestions from './initialQuestions';
+import { create } from "zustand";
+import type { SubjectType } from "~/app/enum/SubjectType";
+import type { HumanBodyPosition } from "~/app/enum/HumanBodyPosition";
+import type { Question } from "~/app/enum/Question";
+import initialQuestions from "./initialQuestions";
 
 interface QuizState {
-    /**
-     * La liste des questions du quiz
-     */
-    questions: Question[];
-    /**
-     * La fonction pour définir si la réponse à une question est correcte ou non
-     * @param questionId L'identifiant de la question
-     * @param isCorrect La réponse à la question est-elle correcte ?
-     * @returns void
-     */
-    setCorrectAnswer: (questionId: number, isCorrect: boolean) => void;
-    /**
-     *
-     * @param id
-     */
-    getQuestionById: (id: number) => Question;
-    /**
-     * La fonction pour obtenir les questions par type de sujet
-     * @param subjectType Le type de sujet des questions
-     * @returns La liste des questions par type de sujet
-     */
-    getQuestionsBySubjectType: (subjectType: SubjectType) => Question[];
-    /**
-     * La fonction pour obtenir la première question par position du corps humain et type de sujet de la question
-     */
-    getFirstQuestionByHumanBodyPositionAndSubjectType(humanBodyPosition: HumanBodyPosition, subjectType: SubjectType): Question | undefined;
-    /**
-     * La fonction pour recommencer le quiz
-     */
-    restartQuiz: () => void;
+  /**
+   * La liste des questions du quiz
+   */
+  questions: Question[];
+  /**
+   * La fonction pour définir si la réponse à une question est correcte ou non
+   * @param questionId L'identifiant de la question
+   * @param isCorrect La réponse à la question est-elle correcte ?
+   * @returns void
+   */
+  setCorrectAnswer: (questionId: number, isCorrect: boolean) => void;
+  /**
+   *
+   * @param id
+   */
+  getQuestionById: (id: number) => Question | undefined;
+  /**
+   * La fonction pour obtenir les questions par type de sujet
+   * @param subjectType Le type de sujet des questions
+   * @returns La liste des questions par type de sujet
+   */
+  getQuestionsBySubjectType: (subjectType: SubjectType) => Question[];
+  /**
+   * La fonction pour obtenir la première question par position du corps humain et type de sujet de la question
+   */
+  getFirstQuestionByHumanBodyPositionAndSubjectType(
+    humanBodyPosition: HumanBodyPosition,
+    subjectType: SubjectType,
+  ): Question | undefined;
+  /**
+   * La fonction pour recommencer le quiz
+   */
+  restartQuiz: () => void;
 }
 
 const useQuizStore = create<QuizState>((set, get) => ({
-    questions: initialQuestions,
-    setCorrectAnswer: (questionId, isCorrect) => {
-        set((state) => {
-            const question = state.questions.find((q) => q.id === questionId);
-            if (question) {
-                question.isCorrect = isCorrect;
-            }
-            return {
-                questions: state.questions,
-            };
-        });
-    },
-    getQuestionById: (idQuestion: number) => {
-        console.log(idQuestion);
-        return get().questions.find((q) => q.id === idQuestion)
-    },
-    getQuestionsBySubjectType: (subjectType: SubjectType) => {
-        return get().questions.filter((q) => q.subjectType === subjectType);
-    },
-    getFirstQuestionByHumanBodyPositionAndSubjectType: (humanBodyPosition: HumanBodyPosition, subjectType: SubjectType) => {
-        return get().questions.find((q) => q.humanBodyPosition === humanBodyPosition && q.subjectType === subjectType);
-    },
-    restartQuiz: () => {
-        set((state) => ({
-            questions: state.questions.map((q) => ({ ...q, isCorrect: undefined })),
-        }));
-    },
+  questions: initialQuestions,
+  setCorrectAnswer: (questionId, isCorrect) => {
+    set((state) => {
+      const question = state.questions.find((q) => q.id === questionId);
+      if (question) {
+        question.isCorrect = isCorrect;
+      }
+      return {
+        questions: state.questions,
+      };
+    });
+  },
+  getQuestionById: (idQuestion: number) => {
+    return get().questions.find((q) => q.id === idQuestion);
+  },
+  getQuestionsBySubjectType: (subjectType: SubjectType) => {
+    return get().questions.filter((q) => q.subjectType === subjectType);
+  },
+  getFirstQuestionByHumanBodyPositionAndSubjectType: (
+    humanBodyPosition: HumanBodyPosition,
+    subjectType: SubjectType,
+  ) => {
+    return get().questions.find(
+      (q) =>
+        q.humanBodyPosition === humanBodyPosition &&
+        q.subjectType === subjectType,
+    );
+  },
+  restartQuiz: () => {
+    set((state) => ({
+      questions: state.questions.map((q) => ({ ...q, isCorrect: undefined })),
+    }));
+  },
 }));
 
 export default useQuizStore;
--- src/components/QCMModal.tsx
+++ src/components/QCMModal.tsx
@@ -7,17 +7,15 @@ import Radar from "~/components/Radar";
 
 interface Props {
   questionId: number;
-  question: string;
-  listReponse: string[];
-  reponse: string;
-  explenation: string;
 }
 
 const QCMModal = (props: Props) => {
   const [selectedAnswer, setSelectedAnswer] = useState("");
   const [isCorrect, setIsCorrect] = useState<boolean | undefined>(undefined);
 
-  const { setCorrectAnswer } = useQuizStore();
+  const { getQuestionById, setCorrectAnswer } = useQuizStore();
+
+  const question = getQuestionById(props.questionId);
 
   const setSelected = (r: string) => {
     setIsCorrect(undefined);
@@ -25,53 +23,58 @@ const QCMModal = (props: Props) => {
   };
 
   const validResponse = () => {
-    if (selectedAnswer === props.reponse) {
-      setIsCorrect(true);
-      setCorrectAnswer(props.questionId, true);
-    } else {
-      setIsCorrect(false);
-      setCorrectAnswer(props.questionId, false);
+    if (question) {
+      if (selectedAnswer === question?.answer) {
+        setIsCorrect(true);
+        setCorrectAnswer(question.id, true);
+      } else {
+        setIsCorrect(false);
+        setCorrectAnswer(question.id, false);
+      }
     }
   };
 
-  const content = (
-    <>
-      <h2 className="pb-5">{props.question}</h2>
-      <ul>
-        {props.listReponse.map((reponse, index) => (
-          <li key={index}>
-            <input
-              type="radio"
-              name="qcm"
-              value={reponse}
-              onClick={() => setSelected(reponse)}
-              checked={reponse === selectedAnswer}
-            />
-            <label className="p-1">{reponse}</label>
-          </li>
-        ))}
-      </ul>
-      <button className="rounded-sm bg-blue-300 p-1" onClick={validResponse}>
-        Valider
-      </button>
-      {isCorrect ? (
-        <p>
-          La réponse est en effet : {props.reponse}
-          <br />
-          {props.explenation}
-        </p>
-      ) : isCorrect == false ? (
-        <p>Mauvaise réponse !</p>
-      ) : (
-        ""
-      )}
-    </>
-  );
-  return (
-    <Radar posX={50} posY={50}>
-      <ModalContent title="QCM" content={content} />
-    </Radar>
-  );
+  if (question) {
+    const content = (
+      <>
+        <h2 className="pb-5">{question?.question}</h2>
+        <ul>
+          {question.multipleAnswers?.map((reponse, index) => (
+            <li key={index}>
+              <input
+                type="radio"
+                name="qcm"
+                value={reponse}
+                onClick={() => setSelected(reponse)}
+                checked={reponse === selectedAnswer}
+              />
+              <label className="p-1">{reponse}</label>
+            </li>
+          ))}
+        </ul>
+        <button className="rounded-sm bg-blue-300 p-1" onClick={validResponse}>
+          Valider
+        </button>
+        {isCorrect ? (
+          <p>
+            La réponse est en effet : {question.answer}
+            <br />
+            {question.explanation}
+          </p>
+        ) : isCorrect == false ? (
+          <p>Mauvaise réponse !</p>
+        ) : (
+          ""
+        )}
+      </>
+    );
+    return (
+      <Radar posX={50} posY={50}>
+        <ModalContent title="QCM" content={content} />
+      </Radar>
+    );
+  }
+  return "";
 };
 
 export default QCMModal;
