822a742457c34d0c6f409d7ea375c8f10a3c94be
Enzo Legros <enzo.legros@isaan.fr>
2024-12-06 07:27:47 +0100

    add question on human side

--- src/app/corps-humain/page.tsx
+++ src/app/corps-humain/page.tsx
@@ -1,32 +1,41 @@
 import ModalContent from "~/components/ModalContent";
+import QCMModal from "~/components/QCMModal";
 import PointRadar from "~/components/Radar";
+import TrueOfFalse from "~/components/TrueOrFalse";
 
 export default function Ocean() {
-    return (
-        <>
-            <video
-                autoPlay loop muted
-                className="object-cover top-0 left-0 fixed z-[-1] w-full h-full opacity-50 blur-sm"
-                src="video/human.mp4"
-            />
-            <div className="flex flex-col items-center justify-center flex-1">
-                <div
-                    style={{
-                        backgroundImage: "url('/img/human.png')",
-                        backgroundSize: "contain",
-                        backgroundPosition: "center center",
-                        backgroundRepeat: "no-repeat",
-                        width: "90vh",
-                        height: "90vh",
-                    }}
-                >
-                </div>
-            </div>
+  return (
+    <>
+      <video
+        autoPlay
+        loop
+        muted
+        className="fixed left-0 top-0 z-[-1] h-full w-full object-cover opacity-50 blur-sm"
+        src="video/human.mp4"
+      />
+      <div className="flex flex-1 flex-col items-center justify-center">
+        <div
+          style={{
+            backgroundImage: "url('/img/human.png')",
+            backgroundSize: "contain",
+            backgroundPosition: "center center",
+            backgroundRepeat: "no-repeat",
+            width: "90vh",
+            height: "90vh",
+          }}
+        >
+          <QCMModal questionId={7} posX={52} posY={13} />
+        </div>
+      </div>
 
-            <div className="fixed bottom-0 right-0 p-2 m-1 bg-black bg-opacity-50 rounded-lg">
-                <a href="https://www.pexels.com/video/scenic-mountain-hiking-adventure-in-washington-29650692/" className="link font-thin">© Scenic Mountain Hiking Adventure in Washington
-                </a>
-            </div>
-        </>
-    );
+      <div className="fixed bottom-0 right-0 m-1 rounded-lg bg-black bg-opacity-50 p-2">
+        <a
+          href="https://www.pexels.com/video/scenic-mountain-hiking-adventure-in-washington-29650692/"
+          className="link font-thin"
+        >
+          © Scenic Mountain Hiking Adventure in Washington
+        </a>
+      </div>
+    </>
+  );
 }
