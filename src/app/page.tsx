e79987a61b3a6b1d95c77fdd789a0cfa84966160
NX <udycz.alexis@gmail.com>
2024-12-06 07:26:58 +0100

    feat: :sparkles: Amélioration page d'acceuil

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,11 @@ export default function HomePage() {
     <div className="flex flex-1 flex-col items-center justify-center bg-[url('/img/background.svg')] bg-cover bg-center text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 backdrop-blur-md">
         <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Explorez <span className="text-[hsl(280,100%,70%)]">les savoirs</span>
+          En apprendre <span className="text-[hsl(280,100%,70%)]">plus</span>
         </h1>
+        <h2 className="text-lg text-center max-w-lg">
+          À propos de nous et des océans. Nous sommes l'équipe <b>EKIP</b>, et en collaboration avec la <a href="https://www.raceforwater.org/" className="link" target="_blank" rel="noopener noreferrer">Fondation Race for Water</a>, nous avons créé ce site pour vous aider à en apprendre davantage sur les océans et le corps humain.
+        </h2>
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
           {/* Section Corps Humain */}
           <div className="flex max-w-md flex-col items-center gap-4 rounded-xl bg-white/10 p-6 text-center text-white hover:bg-white/20">
